9252a9bec819797b17ac71fd069210464f1c4b42
Adí Santos <adi.santos@sequor.com.br>
2022-04-29 15:12:41 -0300

    Aula 4 - 17:44

--- Dominio/Interfaces/INoticia.cs
+++ Dominio/Interfaces/INoticia.cs
@@ -3,6 +3,7 @@ using Entidades.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,6 @@ namespace Dominio.Interfaces
 {
     public interface INoticia : IGenenericos<Noticia>
     {
-        //teste
+        Task<List<Noticia>> ListarNoticias(Expression<Func<Noticia, bool>> exNoticia);
     }
 }
---
+++ Dominio/Interfaces/Servicos/ServicoNoticia.cs
@@ -0,0 +1,38 @@
+﻿using Dominio.Interfaces.InterfaceServicos;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Interfaces.Servicos
+{
+
+    public class ServicoNoticia : IServicoNoticia
+    {
+        private readonly INoticia _INoticia;
+
+
+        public ServicoNoticia(INoticia INoticia)
+        {
+            _INoticia = INoticia;
+        }
+
+
+        public async Task AdicionaNoticia(Noticia noticia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AtualizaNoticia(Noticia noticia)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Noticia>> ListarNoticiasAtivas()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
