f79104304f74d6ec8b3f9c7af1a6404663730cf9
Adí Santos <adi.santos@sequor.com.br>
2022-04-29 14:33:33 -0300

    Commit funcionando

--- Dominio/Interfaces/INoticia.cs
+++ Dominio/Interfaces/INoticia.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//testeasdad
+
 namespace Dominio.Interfaces
 {
     public interface INoticia
     {
-        //testefsdaf
+
     }
 }
-//teste fdssadf2gdfgds
