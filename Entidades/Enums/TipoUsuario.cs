6de74483bd9b3b1cb306abf53c4ad06580a3bf94
Adí Santos <adi.santos@sequor.com.br>
2022-04-29 10:56:40 -0300

    First commit

---
+++ Entidades/Enums/TipoUsuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Enums {
+    public enum TipoUsuario {
+        ADMIN = 1,
+        USER = 2
+    }
+}
